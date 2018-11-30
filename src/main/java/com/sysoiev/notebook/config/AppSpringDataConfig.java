b8f73f5a078fdd04442a96b14da97184c9c3f0f1
Bada1208 <sysoievbogdan@gmail.com>
2018-11-30 06:20:05 +0100

    were fixed spring depencencies and added property file

--- src/main/java/com/sysoiev/notebook/config/AppSpringDataConfig.java
+++ src/main/java/com/sysoiev/notebook/config/AppSpringDataConfig.java
@@ -10,16 +10,16 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@Configuration
-@ComponentScan("com.sysoiev.notebook")
-@PropertySource("classpath:database.properties")
+@Configuration//says that this class is configuration for Spring context.
+@ComponentScan("com.sysoiev.notebook")//specifies the package to scan for component classes.
+@PropertySource("classpath:database.properties")//says that properties will be read from database.properties file.
 public class AppSpringDataConfig {
     @Autowired
     Environment environment;
 
     private final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
     private final String USER = "postgres";
-   // private final String DRIVER = "driver";
+    private final String DRIVER = "driver";
     private final String PASSWORD = "postgres";
 
     @Bean
@@ -28,7 +28,7 @@ public class AppSpringDataConfig {
         driverManagerDataSource.setUrl(environment.getProperty(DB_URL));
         driverManagerDataSource.setUsername(environment.getProperty(USER));
         driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
-       // driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
+        driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
         return driverManagerDataSource;
     }
 }
