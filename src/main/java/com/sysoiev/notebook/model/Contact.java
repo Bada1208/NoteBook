2d58e5bf5a9afc5362d24f25d8cc8e086243abd8
Bada1208 <sysoievbogdan@gmail.com>
2019-06-04 14:54:00 +0300

    added some testing code

--- src/main/java/com/sysoiev/notebook/model/Contact.java
+++ src/main/java/com/sysoiev/notebook/model/Contact.java
@@ -1,45 +1,13 @@
 package com.sysoiev.notebook.model;
 
 
+import com.sysoiev.notebook.services.ContactService;
+import com.sysoiev.notebook.services.impl.ContactServiceImpl;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Objects;
 
 public class Contact {
-
-
-    /**
-     * Класс модель.
-     * Этот класс представляет основные сущности и хранимый тип данных.
-     * Это логические сущности которые управляются слоем сервисов бизнес логики.
-     */
-
-   // private int id;
-    private String surname;
-    private String name;
-    private String phoneNumber;
-    private int age;
-
-    public Contact() {
-
-    }
-
-    public Contact( String surname, String name, String phoneNumber, int age) {
-        //this.id = id;
-        this.surname = surname;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.age = age;
-    }
-/*
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }*/
-
     public String getSurname() {
         return surname;
     }
@@ -72,32 +40,113 @@ public class Contact {
         this.age = age;
     }
 
+    public static ContactService contactService;
+    //ContactServiceImpl contactService;
+    /**
+     * Класс модель.
+     * Этот класс представляет основные сущности и хранимый тип данных.
+     * Это логические сущности которые управляются слоем сервисов бизнес логики.
+     */
 
-   /* @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Contact contact = (Contact) o;
-        return id == contact.id &&
-                age == contact.age &&
-                Objects.equals(surname, contact.surname) &&
-                Objects.equals(name, contact.name) &&
-                Objects.equals(phoneNumber, contact.phoneNumber);*/
-   // }
+    // private int id;
+    private String surname;
+    private String name;
+    private String phoneNumber;
+    private int age;
 
-    /*@Override
-    public int hashCode() {
+    public Contact() {
 
-        return Objects.hash(id, surname, name, phoneNumber, age);
     }
-*/
+
     @Override
     public String toString() {
         return "Contact{" +
-                ", surname='" + surname + '\'' +
+                "surname='" + surname + '\'' +
                 ", name='" + name + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    public Contact(String surname, String name, String phoneNumber, int age) {
+        //this.id = id;
+        if (name != null && !name.isEmpty() && surname != null && !surname.isEmpty() && phoneNumber != null
+                && !phoneNumber.isEmpty() && age > 0) {
+            this.surname = surname;
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.age = age;
+
+        /*
+        if (name != null && !name.isEmpty() && age > 0 && sex != null) {
+            this.name = name;
+            this.age = age;
+            this.sex = sex;
+
+            if (!hasUser()) {
+                countId++;
+                this.id = countId;
+                allUsers.put(id, this);
+            }*/
+        }
+/*
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }*/
+
+        /*public String getSurname () {
+            return surname;
+        }
+
+        public void setSurname (String surname){
+            this.surname = surname;
+        }
+
+        public String getName () {
+            return name;
+        }
+
+        public void setName (String name){
+            this.name = name;
+        }
+
+        public String getPhoneNumber () {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber (String phoneNumber){
+            this.phoneNumber = phoneNumber;
+        }
+
+        public int getAge () {
+            return age;
+        }
+
+        public void setAge ( int age){
+            this.age = age;
+        }
+*/
+
+   /* @Override
+    public boolean equals(Contact c) {
+        if (this == c) return true;
+        if (c == null || getClass() != c.getClass()) return false;
+        Contact contact = (Contact) c;
+        return age == contact.age &&
+                Objects.equals(surname, contact.surname) &&
+                Objects.equals(name, contact.name) &&
+                Objects.equals(phoneNumber, contact.phoneNumber);
+        // }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash( surname, name, phoneNumber, age);
+    }*/
+
     }
 }
--- src/test/java/com/sysoiev/notebook/services/impl/ContactServiceImplTest.java
+++ src/test/java/com/sysoiev/notebook/services/impl/ContactServiceImplTest.java
@@ -1,12 +1,21 @@
 package com.sysoiev.notebook.services.impl;
 
+import com.sysoiev.notebook.model.Contact;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactServiceImplTest extends Assert {
-    ContactServiceImpl contactService = new ContactServiceImpl();
+   public ContactServiceImpl contactService;// = new ContactServiceImpl();
+    private Contact contact1;
+    private Contact contact2;
+
+    private Contact contactNotAdd1;
+    private Contact contactNotAdd2;
 
     @BeforeClass
     public static void setUpBeforeClass() {
@@ -16,22 +25,51 @@ public class ContactServiceImplTest extends Assert {
     @Before
     public void setUpBefore() {
         System.out.println("@Before annotation");
+        contact1 = new Contact("Sysoiev", "Bogdan", "123456789", 33);
+        contact2 = new Contact("Sysoieva", "Anna", "987654321", 26);
+
+        contactNotAdd1 = new Contact("", "", "", 0);
+        contactNotAdd2 = new Contact("", "", "", 0);
     }
 
     @Test
     public void createContactTest(String surname, String name, String phoneNumber, int age) {
-        assertNotNull(createContact("Sysoiev","Bogdan","123456789",33));
+       // assertNotNull(createContact("Sysoiev", "Bogdan", "123456789", 33));
     }
+    /*//создаем тестовые данные
+    User user = new User("Евгений", 35, Sex.MALE);
+    User user1 = new User("Марина", 34, Sex.FEMALE);
+    User user2 = new User("Алина", 7, Sex.FEMALE);
+
+    //создаем список expected и заполняем его данными нашего метода
+    List<User> expected = User.getAllUsers();
+
+    //создаем список actual в него помещаем данные для сравнения
+    //то что мы предпологиаем метод должен вернуть
+    List<User> actual = new ArrayList<>();
+    actual.add(user);
+    actual.add(user1);
+    actual.add(user2);
+
+    //запускаем тест, в случае если список expected и actual не будут равны
+    //тест будет провален, о результатах теста читаем в консоли
+    Assert.assertEquals(expected, actual);*/
 
 
     @Test
-    public String deleteContactTest(String surname) {
-        assertNotNull(deleteContactTest("Sysoiev"));
+    public void deleteContactTest(String surname) {
+        //assertNotNull(deleteContactTest("Sysoiev"));
     }
 
     @Test
     public void showAllContactsTest() {
-        assertNotNull(contactService);
+        List<Contact> expected = Contact.contactService.showAllContacts();
+
+        List<Contact> actual = new ArrayList<>();
+        actual.add(contact1);
+        actual.add(contact2);
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
